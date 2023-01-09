e2f9f5938a96672d8bcf90babf98b0f8962c64da
Kevin Old <kevin@kevinold.com>
2023-01-09 11:56:13 -0600

    add links to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import AComp from "./AComp";
 import BComp from "./BComp";
 
 function App() {
   return (
     <div>
+      <Link to="/a" data-test="a-link">Navigate to A</Link>
+      <Link to="/b" data-test="b-link">Navigate to B</Link>
       <Routes>
         <Route path="/a" element={<AComp />} />
         <Route path="/b" element={<BComp />} />

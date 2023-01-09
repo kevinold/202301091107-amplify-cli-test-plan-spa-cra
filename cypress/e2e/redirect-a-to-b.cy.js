e3c79b1271725c09d367cf48541f64937ee7133f
Kevin Old <kevin@kevinold.com>
2023-01-09 12:28:06 -0600

    configure cypress

---
+++ cypress/e2e/redirect-a-to-b.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe("301 redirects", () => {
+  it("should redirect /a to /b", () => {
+    cy.visit("/a");
+    cy.getBySel("content").should("have.text", "This is the B route");
+  });
+});
